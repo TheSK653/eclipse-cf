976010c927639588252c8130a6477c49f20ae97f
Soham Kundu <153625511+TheSK653@users.noreply.github.com>
2026-07-21 09:52:04 +0530

    class 2nd q

---
+++ hackerrank/Priyanka_and_Toys.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Priyanka_and_Toys {
+
+    /*
+     * Complete the 'toys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY w as parameter.
+     */
+
+    public static int toys(List<Integer> w) {
+    // Write your code here
+        Collections.sort(w);
+        int p=w.get(0)+4,ans=1;
+        for(int x:w){
+            if(x>p){
+                ans++;
+                p=x+4;
+            }
+        }
+        return ans;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> w = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.toys(w);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
